a823ffcd52cdc05ae03243ef227072aaf4dde067
Cameron Moodley <moodley.cameron@gmail.com>
2021-08-03 09:36:10 +0200

    tests: added in tests for validate function

--- client/src/features/login/login.spec.ts
+++ client/src/features/login/login.spec.ts
@@ -1,1 +1,12 @@
-export {};
+import Login from './Login';
+import { validateEmail } from './loginHelpers';
+
+describe('Login', () => {
+	test('validateEmail function should validate emails', () => {
+		expect(validateEmail('cameron.moodley@me.com')).toBe(true);
+	});
+
+	test('validateEmail function should validate emails', () => {
+		expect(validateEmail('cameron')).toBe(false);
+	});
+});
