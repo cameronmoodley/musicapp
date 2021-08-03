9b7e3ebe83838d19af623ed4d02fca2d0dc99a6c
Cameron Moodley <moodley.cameron@gmail.com>
2021-08-03 07:47:32 +0200

    code: initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
+app.use('/api/auth', require('./routes/api/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`);
+});
